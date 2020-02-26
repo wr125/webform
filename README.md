821c5e360575f3c13f59a404feaa11dd9d26d89e
Robert Willis <robert.willis010@gmail.com>
2020-02-26 17:06:53 +0000

    Divided Results

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # webform
+
+A simple input web form based on speed divided by distance in Go.
+
+#Convert a string type input to a float64 output.
+#Using go templates.
--- main.go
+++ main.go
@@ -1,70 +1,69 @@
 package main
 
-import ("fmt"
-"log"
-"net/http"
-"html/template"
-"strconv"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 var tpl *template.Template
 
 func init() {
-    tpl = template.Must(template.ParseGlob("Template/*.html"))
+	tpl = template.Must(template.ParseGlob("Template/*.html"))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-    err := r.ParseForm()
-    if err != nil{
-      log.Fatal(err)
-    }
-    for key, value := range r.Form{
-      fmt.Printf("%s\n %v\n", key, value)
-    }
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for key, value := range r.Form {
+		fmt.Printf("%s\n %v\n", key, value)
+	}
 
-    tpl.ExecuteTemplate(w, "index.html", nil)
+	tpl.ExecuteTemplate(w, "index.html", nil)
 }
 
+type car struct {
+	Speed    float64
+	Distance float64
+}
 
+func (c car) Acceleration() float64 {
+	return c.Speed / c.Distance
+}
 
 func results(w http.ResponseWriter, r *http.Request) {
 
-    if r.Method == "Post" {
-        http.Redirect(w, r, "/", http.StatusSeeOther)
-        return
+	if r.Method == "Post" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
 
-}
-speed, err := strconv.ParseFloat(r.FormValue("speed")[0:], 10)
-if err != nil {
-  log.Fatal(err)
-}
-distance, err := strconv.ParseFloat(r.FormValue("distance")[0:], 10)
-if err != nil {
-  log.Fatal(err)
-}
-//example of wanting to show results on the html/template
-fmt.Printf("%v\n", speed / distance)
+	}
 
+	speed, err := strconv.ParseFloat(r.FormValue("speed")[0:], 10)
+	if err != nil {
+		log.Fatal(err)
+	}
+	distance, err := strconv.ParseFloat(r.FormValue("distance")[0:], 10)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-car := struct{
-  Speed float64
-  Distance float64
-}{
-  Speed: speed,
-  Distance: distance,
-}
+	car := car{Speed: speed, Distance: distance}
 
-tpl.ExecuteTemplate(w, "results.html", car)
-
+	tpl.ExecuteTemplate(w, "results.html", car)
 
 }
 func main() {
 
-  mux := http.NewServeMux()
-      mux.HandleFunc("/", index)
-      mux.HandleFunc("/results", results)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/results", results)
 
-      fmt.Println("server is running..")
-      err := http.ListenAndServe(":4000", mux)
-      log.Fatal(err)
+	fmt.Println("server is running..")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
